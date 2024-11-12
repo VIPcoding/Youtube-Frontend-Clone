74dd16196c64330a4f82d73c0478d3b9042cf7bd
VIPcoding <vipsachdeva1@gmail.com>
2024-11-12 13:53:20 +0530

    first commit

---
+++ src/Components/Fedd/Feed.jsx
@@ -0,0 +1,118 @@
+// import React from 'react'
+import "./Feed.css"
+import thumnail1 from "../../assets/thumbnail1.png"
+import thumnail2 from "../../assets/thumbnail2.png"
+import thumnail3 from "../../assets/thumbnail3.png"
+import thumnail4 from "../../assets/thumbnail4.png"
+import thumnail5 from "../../assets/thumbnail5.png"
+import thumnail6 from "../../assets/thumbnail6.png"
+import thumnail7 from "../../assets/thumbnail7.png"
+import thumnail8 from "../../assets/thumbnail8.png"
+import { Link } from 'react-router-dom'
+
+
+
+const Feed = () => {
+  return (
+    <div className='feed'>
+        <Link to={`video/20/4521`} className="card">
+            <img src={thumnail1} alt="" />
+            <h2>Best Channel to learn Coding that help you to be a developer</h2>
+            <h3>Grenn Yeelow</h3>
+            <p>15k vies &bull; 2 days ago</p>
+        </Link>
+        <div className="card">
+            <img src={thumnail2} alt="" />
+            <h2>Best Channel to learn Coding that help you to be a developer</h2>
+            <h3>Grenn Yeelow</h3>
+            <p>15k vies &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src={thumnail3} alt="" />
+            <h2>Best Channel to learn Coding that help you to be a developer</h2>
+            <h3>Grenn Yeelow</h3>
+            <p>15k vies &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src={thumnail4} alt="" />
+            <h2>Best Channel to learn Coding that help you to be a developer</h2>
+            <h3>Grenn Yeelow</h3>
+            <p>15k vies &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src={thumnail5} alt="" />
+            <h2>Best Channel to learn Coding that help you to be a developer</h2>
+            <h3>Grenn Yeelow</h3>
+            <p>15k vies &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src={thumnail6} alt="" />
+            <h2>Best Channel to learn Coding that help you to be a developer</h2>
+            <h3>Grenn Yeelow</h3>
+            <p>15k vies &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src={thumnail7} alt="" />
+            <h2>Best Channel to learn Coding that help you to be a developer</h2>
+            <h3>Grenn Yeelow</h3>
+            <p>15k vies &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src={thumnail8} alt="" />
+            <h2>Best Channel to learn Coding that help you to be a developer</h2>
+            <h3>Grenn Yeelow</h3>
+            <p>15k vies &bull; 2 days ago</p>
+        </div> 
+        <div className="card">
+            <img src={thumnail1} alt="" />
+            <h2>Best Channel to learn Coding that help you to be a developer</h2>
+            <h3>Grenn Yeelow</h3>
+            <p>15k vies &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src={thumnail2} alt="" />
+            <h2>Best Channel to learn Coding that help you to be a developer</h2>
+            <h3>Grenn Yeelow</h3>
+            <p>15k vies &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src={thumnail3} alt="" />
+            <h2>Best Channel to learn Coding that help you to be a developer</h2>
+            <h3>Grenn Yeelow</h3>
+            <p>15k vies &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src={thumnail4} alt="" />
+            <h2>Best Channel to learn Coding that help you to be a developer</h2>
+            <h3>Grenn Yeelow</h3>
+            <p>15k vies &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src={thumnail5} alt="" />
+            <h2>Best Channel to learn Coding that help you to be a developer</h2>
+            <h3>Grenn Yeelow</h3>
+            <p>15k vies &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src={thumnail6} alt="" />
+            <h2>Best Channel to learn Coding that help you to be a developer</h2>
+            <h3>Grenn Yeelow</h3>
+            <p>15k vies &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src={thumnail7} alt="" />
+            <h2>Best Channel to learn Coding that help you to be a developer</h2>
+            <h3>Grenn Yeelow</h3>
+            <p>15k vies &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src={thumnail8} alt="" />
+            <h2>Best Channel to learn Coding that help you to be a developer</h2>
+            <h3>Grenn Yeelow</h3>
+            <p>15k vies &bull; 2 days ago</p>
+        </div> 
+    </div>
+  )
+}
+
+export default Feed
